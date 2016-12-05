2a548bbf5d5fb10ba6cfd6deddf1b76aca878de4
Tobias Richter <tr4711@gmail.com>
2016-12-05 16:22:52 +0100

    test(bnb_problem) use generated example model instead of own

--- test/core/bnb_problem_class_test.cc
+++ test/core/bnb_problem_class_test.cc
@@ -2,31 +2,16 @@
 #include <ilcplex/ilocplex.h>
 #include "gtest/gtest.h"
 #include "core/bnb_problem_class.h"
+#include "example_model/example_cplex_model_class.h"
 
 using namespace std;
 
 TEST(BnBProblem, getSolution) {
   try {
-    IloEnv environment;
-    IloModel model(environment);
-    IloNumVarArray variables(environment);
 
-    variables.add(IloNumVar(environment, 0.0, 1.0, ILOFLOAT));
-    variables.add(IloNumVar(environment, 0.0, 2.0, ILOFLOAT));
-    variables.add(IloNumVar(environment, 0.0, 3.0, ILOFLOAT));
-    variables.add(IloNumVar(environment, 0.0, IloInfinity, ILOFLOAT));
-    variables.add(IloNumVar(environment, 0.0, IloInfinity, ILOFLOAT));
+    ExampleCplexModel example_model;
+    BnBProblem problem(example_model.model, example_model.variables);
 
-    model.add(IloMaximize(environment, 2 * variables[3] + 3 * variables[4] + variables[0] + variables[1] + variables[2]));
-
-    IloRangeArray constraints(environment);
-    constraints.add(4 * variables[3] + 12 * variables[4] <= 33);
-    constraints.add(10 * variables[3] + 4 * variables[4] <= 35);
-    constraints[0].setName("c1");
-    constraints[1].setName("c2");
-    model.add(constraints);
-
-    BnBProblem problem(&model, &variables);
     problem.Solve();
     const IloNumArray solution = problem.GetSolution();
     ASSERT_EQ(1, solution[0]);

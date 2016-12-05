63265a438ae122be3eff531d542c865122651c79
Tobias Richter <tr4711@gmail.com>
2016-12-05 16:03:42 +0100

    test(exampleproblem) added sample problem to reuse in test

--- test/test_util/test_cplex_problem_class.cc
+++ test/test_util/test_cplex_problem_class.cc
@@ -7,39 +7,28 @@ using namespace std;
 
 TestCplexProblem::TestCplexProblem() {
 
-  IloEnv environment_;
-  this->environment = &environment_;
+  this->environment = new IloEnv();
+  this->model = new IloModel(*environment); 
+  this->variables = new IloNumVarArray(*environment);
 
-  IloModel model_(environment_);
-  this->model = &model_;
+  variables->add(IloNumVar(*environment, 0.0, 1.0, ILOFLOAT));
+  variables->add(IloNumVar(*environment, 0.0, 2.0, ILOFLOAT));
+  variables->add(IloNumVar(*environment, 0.0, 3.0, ILOFLOAT));
+  variables->add(IloNumVar(*environment, 0.0, IloInfinity, ILOFLOAT));
+  variables->add(IloNumVar(*environment, 0.0, IloInfinity, ILOFLOAT));
 
-  IloNumVarArray variables_(environment_);
-  this->variables = &variables_;
+  IloRangeArray constraints(*environment);
 
-  variables_.add(IloNumVar(environment_, 0.0, 1.0, ILOFLOAT));
-  variables_.add(IloNumVar(environment_, 0.0, 2.0, ILOFLOAT));
-  variables_.add(IloNumVar(environment_, 0.0, 3.0, ILOFLOAT));
-  variables_.add(IloNumVar(environment_, 0.0, IloInfinity, ILOFLOAT));
-  variables_.add(IloNumVar(environment_, 0.0, IloInfinity, ILOFLOAT));
-
-  IloRangeArray constraints(environment_);
-
-  model_.add(IloMaximize(environment_, 2 * variables_[3] + 3 * variables_[4] + variables_[0] + variables_[1] + variables_[2]));
+  // fix to support '*' in maximize statement
+  IloNumVarArray variables_ = *this->variables;
+  model->add(IloMaximize(*environment, 2 * variables_[3] + 3 * variables_[4] + variables_[0] + variables_[1] + variables_[2]));
 
   constraints.add(4 * variables_[3] + 12 * variables_[4] <= 33);
   constraints.add(10 * variables_[3] + 4 * variables_[4] <= 35);
-
   constraints[0].setName("c1");
   constraints[1].setName("c2");
 
-  model_.add(constraints);
-
-  IloCplex cplex_(model_);
-  this->cplex = &cplex_;
-
+  model->add(constraints);
+  this->cplex = new IloCplex(*model);
 }
-
-
-
-
 

eb5a61e211df1f916a7d06cf3945f9a34ac143d6
Marvin Schirrmacher <schirrmacher@icloud.com>
2016-12-05 16:14:09 +0100

    refac(cplex_util) remove cplex util

--- src/util/cplex_util.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef BNB_UTIL_CPLEX_UTIL_H_
-#define BNB_UTIL_CPLEX_UTIL_H_
-
-#include <ilcplex/ilocplex.h>
-
-const double *ExtractSolutions(IloCplex &cplex, IloNumVarArray &variables);
-
-#endif //BNB_UTIL_CPLEX_UTIL_H_

a936fd4fb1176c18a786a345d76c8767471a0db6
Marvin Schirrmacher <schirrmacher@icloud.com>
2016-12-07 19:45:56 +0100

    feat(branch_and_bound) add optimize implementation

--- src/core/branch_and_bound_class.cc
+++ src/core/branch_and_bound_class.cc
@@ -1,4 +1,7 @@
+
+#include <vector>
 #include "branch_and_bound_class.h"
+#include "branching/branching.h"
 #include "node_selection/bnb_node_selection_interface.h"
 #include "node_selection/depth_first_traversal_class.h"
 #include "core/bnb_problem_class.h"
@@ -27,9 +30,7 @@ BranchAndBound::BranchAndBound(IloModel* model, IloNumVarArray* variables) {
  * TODO: Where to remove contraints from model
  */
 void BranchAndBound::optimize() {
-  /*
-   * 0.generate root problem and add to node selection
-   */
+
   BnBProblem problem(&cplex_, variables_);
   Node<BnBProblem*> root(&problem);
 
@@ -37,28 +38,39 @@ void BranchAndBound::optimize() {
   node_selection.SetNextNode(&root);
 
   while (node_selection.HasNextNode()) {
-    //1. get next node from NodeSelection
+
     Node<BnBProblem*> actual_node = *node_selection.next_node();
     BnBProblem actual_problem = *actual_node.content;
 
-    // 2. solve BnBProblem of Node and find optimal solution z for variables x*/
-    // IloNumArray actual_solution = actual_problem->GetSolution();
-    // TODO: find objective value/solution 'z'
     actual_problem.Solve();
 
-    // 3. if infeasible or unbounded process next node
-    // if (actual_problem->infeasible() || actual_problem->unbounded()) {
-    //   continue;
-    // }
+    IloNumArray actual_solution = actual_problem.GetSolution();
 
-    // 4. if actual_solution integer values, set global_lower_bound LB to max{LB, z} and process next node;
+    // find optimal solution z for variables x*/
+    // TODO: find objective value/solution 'z'
 
-    // 5. if z <= LB and process next node
+    if (actual_problem.infeasible() || actual_problem.unbounded()) {
+      continue;
+    }
 
-    /*   6.  - get (next) variable to fixate from BranchingRule
-     *        - generate two (sub-) BnBProblems with Constraints from BranchingRule
-     *        - add new Problems to NodeSelection
+    Branching branching(FIRST_FRACTIONAL);
+    std::vector<BnBProblem*> branches = *branching.Branch(cplex_, actual_solution, *variables_);
+
+    if (branches.size() == 0) {
+      // set global_lower_bound LB to max{LB, z} and process next node;
+      continue;
+    }
+    // if z <= LB and process next node
+
+    /*  - get (next) variable to fixate from BranchingRule
+     *  - generate two (sub-) BnBProblems with Constraints from BranchingRule
+     *  - add new Problems to NodeSelection
      */
+
+    for (auto branch : branches) {
+      Node<BnBProblem*> *new_problem = new Node<BnBProblem*>(branch);
+      //node_selection.SetNextNode(new_problem);
+    }
   }
 }
 

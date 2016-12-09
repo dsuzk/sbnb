d04513098149e05987a8a2e4e2a69d945cc4322b
Tobias Richter <tr4711@gmail.com>
2016-12-09 13:51:12 +0100

    test(branching) use example model instead of own

--- test/branching/branching_test.cc
+++ test/branching/branching_test.cc
@@ -2,38 +2,19 @@
 #include <gtest/gtest.h>
 #include <ilcplex/ilocplex.h>
 #include <branching/branching.h>
+#include "example_model/example_cplex_model_class.h"
 
 TEST(Branching, branch_Test) {
 
-  IloEnv environment;
-  IloModel model(environment);
-  IloNumVarArray variables(environment);
-
-  variables.add(IloNumVar(environment, 0.0, IloInfinity, ILOFLOAT));
-  variables.add(IloNumVar(environment, 0.0, IloInfinity, ILOFLOAT));
-
-  model.add(IloMaximize(environment, 2 * variables[0] + 3 * variables[1]));
-
-  IloRangeArray constraints(environment);
-  constraints.add(4 * variables[0] + 12 * variables[1] <= 33);
-  constraints[0].setName("c1");
-
-  constraints.add(10 * variables[0] + 4 * variables[1] <= 35);
-  constraints[1].setName("c2");
-
-  model.add(constraints);
-
-  IloCplex cplex(model);
-  cplex.setOut(environment.getNullStream());
-
+  ExampleCplexModel example_model;
   // ACTUAL TEST ---------------------------------------------------
 
-  OptimizationProblem root_problem(&cplex, &variables);
+  OptimizationProblem root_problem(example_model.cplex, example_model.variables);
   root_problem.Solve();
   IloNumArray solution = root_problem.GetSolution();
 
   Branching branching(BranchingRule::FIRST_FRACTIONAL);
-  const vector<OptimizationProblem *> sub_problems = *branching.Branch(cplex, solution, variables);
+  const vector<OptimizationProblem *> sub_problems = *branching.Branch(*example_model.cplex, solution, *example_model.variables);
 
   double expected_solution_of_branch1[2] = {3.0, 1.25};
   double expected_solution_of_branch2[2] = {2.0, 2.0833333333333335};
@@ -54,5 +35,5 @@ TEST(Branching, branch_Test) {
     }
   }
 
-  environment.end();
+  example_model.environment->end();
 }

04d08ce603e090829c22bc49f478d38cd21cf305
Niklas Reinhold <nreinhold24@gmail.com>
2016-12-09 12:39:27 +0100

    test(bnb_problem) add GetObjectiveValue test

--- test/core/bnb_problem_class_test.cc
+++ test/core/bnb_problem_class_test.cc
@@ -92,3 +92,40 @@ TEST(BnBProblem, unbounded) {
     cerr << "Error: " << e.getMessage() << endl;
   }
 }
+
+TEST(BnBProblem, objective_value) {
+  try {
+
+    IloEnv environment;
+    IloModel model(environment);
+    IloNumVarArray variables(environment);
+
+    variables.add(IloNumVar(environment, 0.0, IloInfinity, ILOFLOAT));
+    variables.add(IloNumVar(environment, 0.0, IloInfinity, ILOFLOAT));
+
+    model.add(IloMaximize(environment, 2 * variables[0] + 3 * variables[1]));
+
+    IloRangeArray constraints(environment);
+    constraints.add(4 * variables[0] + 12 * variables[1] <= 33);
+    constraints[0].setName("c1");
+
+    constraints.add(10 * variables[0] + 4 * variables[1] <= 35);
+    constraints[1].setName("c2");
+
+    model.add(constraints);
+
+    IloCplex cplex(model);
+    cplex.setOut(environment.getNullStream());
+
+
+    BnBProblem problem(&cplex, &variables);
+    problem.Solve();
+
+    double expected_solution_ = 11.01923076923077;
+
+    ASSERT_DOUBLE_EQ(expected_solution_, problem.GetObjectiveValue());
+
+  } catch (const IloException& e) {
+    cerr << "Error: " << e.getMessage() << endl;
+  }
+}

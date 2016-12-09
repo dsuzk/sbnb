25304059c8f9338bca114eea396be01d57a0af1e
Tobias Richter <tr4711@gmail.com>
2016-12-09 14:28:59 +0100

    test(bnb_problem) remove try catch and use example model

--- test/core/bnb_problem_class_test.cc
+++ test/core/bnb_problem_class_test.cc
@@ -1,14 +1,8 @@
-#include <iostream>
-#include <ilcplex/ilocplex.h>
 #include "gtest/gtest.h"
 #include "core/optimization_problem_class.h"
 #include "example_model/example_cplex_model_class.h"
 
-using namespace std;
-
 TEST(OptimizationProblem, getSolution) {
-  try {
-
     ExampleCplexModel example_model;
     OptimizationProblem problem(example_model.cplex, example_model.variables);
 
@@ -16,15 +10,9 @@ TEST(OptimizationProblem, getSolution) {
     const IloNumArray solution = problem.GetSolution();
     ASSERT_DOUBLE_EQ(2.7692307692307692, solution[0]);
     ASSERT_DOUBLE_EQ(1.8269230769230766, solution[1]);
-
-  } catch (const IloException& e) {
-    cerr << "Error: " << e.getMessage() << endl;
-  }
 }
 
 TEST(OptimizationProblem, solved) {
-  try {
-
     ExampleCplexModel example_model;
     OptimizationProblem problem(example_model.cplex, example_model.variables);
 
@@ -33,15 +21,9 @@ TEST(OptimizationProblem, solved) {
     ASSERT_TRUE(problem.IsSolved());
     ASSERT_FALSE(problem.IsInfeasible());
     ASSERT_FALSE(problem.IsUnbounded());
-
-  } catch (const IloException& e) {
-    cerr << "Error: " << e.getMessage() << endl;
-  }
 }
 
 TEST(OptimizationProblem, infeasible) {
-  try {
-
     IloEnv env;
     IloModel model(env);
     IloNumVarArray variables(env);
@@ -55,30 +37,15 @@ TEST(OptimizationProblem, infeasible) {
     model.add(constraints);
 
     IloCplex cplex(model);
-<<<<<<< HEAD
     OptimizationProblem problem(&cplex, &variables);
     problem.Solve();
 
     ASSERT_TRUE(problem.IsInfeasible());
-=======
-    cplex.setOut(env.getNullStream());
-
-    BnBProblem problem(&cplex, &variables);
-    problem.Solve();
-
-    ASSERT_TRUE(problem.infeasible());
-    ASSERT_FALSE(problem.unbounded());
-    ASSERT_FALSE(problem.solved());
->>>>>>> test(bnb_problem) assert on side effects
-
-  } catch (const IloException& e) {
-    cerr << "Error: " << e.getMessage() << endl;
-  }
+    ASSERT_FALSE(problem.IsUnbounded());
+    ASSERT_FALSE(problem.IsSolved());
 }
 
 TEST(OptimizationProblem, unbounded) {
-  try {
-
     IloEnv env;
     IloModel model(env);
     IloNumVarArray variables(env);
@@ -98,53 +65,18 @@ TEST(OptimizationProblem, unbounded) {
     OptimizationProblem problem(&cplex, &variables);
     problem.Solve();
 
-<<<<<<< HEAD
     ASSERT_TRUE(problem.IsUnbounded());
-=======
-    ASSERT_TRUE(problem.unbounded());
-    ASSERT_FALSE(problem.infeasible());
-    ASSERT_FALSE(problem.solved());
->>>>>>> test(bnb_problem) assert on side effects
-
-  } catch (const IloException& e) {
-    cerr << "Error: " << e.getMessage() << endl;
-  }
+    ASSERT_FALSE(problem.IsInfeasible());
+    ASSERT_FALSE(problem.IsSolved());
 }
 
 TEST(OptimizationProblem, objective_value) {
-  try {
-
-    IloEnv environment;
-    IloModel model(environment);
-    IloNumVarArray variables(environment);
-
-    variables.add(IloNumVar(environment, 0.0, IloInfinity, ILOFLOAT));
-    variables.add(IloNumVar(environment, 0.0, IloInfinity, ILOFLOAT));
-
-    model.add(IloMaximize(environment, 2 * variables[0] + 3 * variables[1]));
-
-    IloRangeArray constraints(environment);
-    constraints.add(4 * variables[0] + 12 * variables[1] <= 33);
-    constraints[0].setName("c1");
-
-    constraints.add(10 * variables[0] + 4 * variables[1] <= 35);
-    constraints[1].setName("c2");
-
-    model.add(constraints);
-
-    IloCplex cplex(model);
-    cplex.setOut(environment.getNullStream());
-
-
-    OptimizationProblem problem(&cplex, &variables);
+    ExampleCplexModel example_model;
+    OptimizationProblem problem(example_model.cplex, example_model.variables);
     problem.Solve();
 
     double expected_solution_ = 11.01923076923077;
 
     ASSERT_DOUBLE_EQ(expected_solution_, problem.GetObjectiveValue());
-
-  } catch (const IloException& e) {
-    cerr << "Error: " << e.getMessage() << endl;
-  }
 }
 

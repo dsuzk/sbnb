332364d6beaeef87952642752d81c416df1494eb
Tobias Richter <tr4711@gmail.com>
2016-12-09 12:25:34 +0100

    test(branching) test root node too

--- test/branching/branching_test.cc
+++ test/branching/branching_test.cc
@@ -26,39 +26,33 @@ TEST(Branching, branch_Test) {
   IloCplex cplex(model);
   cplex.setOut(environment.getNullStream());
 
-  // Solution for x1 => 2.769231
-  // Solution for x2 => 1.826923
-  cplex.solve();
-  IloNumArray solutions_(environment);
-  cplex.getValues(solutions_,variables );
-
   // ACTUAL TEST ---------------------------------------------------
 
+  OptimizationProblem root_problem(&cplex, &variables);
+  root_problem.Solve();
+  IloNumArray solution = root_problem.GetSolution();
+
   Branching branching(BranchingRule::FIRST_FRACTIONAL);
-  const vector<OptimizationProblem *> sub_problems = *branching.Branch(cplex, solutions_, variables);
+  const vector<OptimizationProblem *> sub_problems = *branching.Branch(cplex, solution, variables);
 
-  double expected_solutions_of_branch1[2] = {3.0, 1.25};
-  double expected_solutions_of_branch2[2] = {2.0, 2.0833333333333335};
+  double expected_solution_of_branch1[2] = {3.0, 1.25};
+  double expected_solution_of_branch2[2] = {2.0, 2.0833333333333335};
 
-  double *expected_solutions[2] = {expected_solutions_of_branch1, expected_solutions_of_branch2};
+  double *expected_solution[2] = {expected_solution_of_branch1, expected_solution_of_branch2};
 
   for (int i = 0; i < sub_problems.size(); ++i) {
     OptimizationProblem sub_problem = *sub_problems[i];
 
     IloConstraintArray fixings = sub_problem.GetFixings();
-    model.add(fixings);
 
     sub_problem.Solve();
-    IloNumArray solutions = sub_problem.GetSolution();
+    solution = sub_problem.GetSolution();
 
-    for (int j = 0; j < solutions.getSize(); ++j) {
-      double actual_solution = solutions[j];
-      EXPECT_DOUBLE_EQ(actual_solution, expected_solutions[i][j]);
+    for (int j = 0; j < solution.getSize(); ++j) {
+      double actual_solution = solution[j];
+      ASSERT_DOUBLE_EQ(actual_solution, expected_solution[i][j]);
     }
-
-    model.remove(fixings);
   }
 
   environment.end();
-
 }

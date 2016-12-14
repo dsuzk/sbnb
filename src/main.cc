c9563307313f22ecef8f7931ec8499a433b35619
Niklas Reinhold <nreinhold24@gmail.com>
2016-12-14 15:03:31 +0100

    feat(main) add cli functionality

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,28 @@
+#include <iostream>
+#include <core/branch_and_bound_class.h>
 
-int main(int, char **) {
+int main(int argn, char* argv[]) {
+  const char* lp_file_path = argv[1];
+  IloEnv env;
+  IloModel model(env);
+  IloNumVarArray vars(env);
+  IloCplex cplex(model);
+  IloRangeArray constraints(env);
+  IloObjective objective;
+  BranchAndBound bnb(&model, &vars);
 
-    return 0;
+  try {
+    cplex.importModel(model, lp_file_path, objective, vars, constraints);
+    bnb.optimize();
+  }
+  catch (IloException& e){
+    cerr << "Exception caught: " << e <<endl;
+  }
+  std::cout << " ------- Branch And Bound Summary ------- " << endl;
+  std::cout << "Objective Value: " << bnb.GetGlobalDualBound() << endl;
+  std::cout << "Variable Values: " << bnb.GetBestSolution() << endl;
 
+  env.end();
+
+  return 0;
 }

236e3eaa945e73598a06a438281bfa614a0c2c9c
Tobias Richter <tr4711@gmail.com>
2016-12-20 17:21:51 +0100

    test(branch_and_bound) add test for location.lp and qpex.lp

--- test/core/branch_and_bound_class_test.cc
+++ test/core/branch_and_bound_class_test.cc
@@ -62,3 +62,58 @@ TEST(BranchAndBoundClass, relaxation_test) {
   model_loader.environment->end();
 }
   
+TEST(ModelTest, location_lp) {
+  char* model_path = (char*)"test/test_models/location.lp";
+  TestModelLoader model_loader(model_path);
+
+  BranchAndBound bnb(model_loader.model, model_loader.variables);
+  bnb.optimize();
+  IloNumArray solution_values = bnb.GetBestSolution();
+  double objective_value = bnb.GetGlobalDualBound();
+
+  TestModelLoader model_loader_expected(model_path);
+  model_loader_expected.model->add(IloConversion(*model_loader.environment, *model_loader.variables, ILOINT));
+  model_loader_expected.cplex->solve();
+
+  IloNumArray expected_solution_values(*model_loader_expected.environment);
+  model_loader_expected.cplex->getValues(expected_solution_values, *model_loader_expected.variables);
+  double expected_objective_value = model_loader_expected.cplex->getObjValue();
+
+  ASSERT_DOUBLE_EQ(model_loader_expected.cplex->getObjValue(), objective_value);
+
+  for (int i = 0; i < solution_values.getSize(); ++i) {
+    IloNum actual_solution_value = solution_values[i];
+    IloNum expected_solution_value = expected_solution_values[i];
+    ASSERT_EQ(actual_solution_value, expected_solution_value);
+  }
+
+  model_loader.environment->end();
+}
+
+TEST(ModelTest, qpex_lp) {
+  char* model_path = (char*)"test/test_models/qpex.lp";
+  TestModelLoader model_loader(model_path);
+
+  BranchAndBound bnb(model_loader.model, model_loader.variables);
+  bnb.optimize();
+  IloNumArray solution_values = bnb.GetBestSolution();
+  double objective_value = bnb.GetGlobalDualBound();
+
+  TestModelLoader model_loader_expected(model_path);
+  model_loader_expected.model->add(IloConversion(*model_loader.environment, *model_loader.variables, ILOINT));
+  model_loader_expected.cplex->solve();
+
+  IloNumArray expected_solution_values(*model_loader_expected.environment);
+  model_loader_expected.cplex->getValues(expected_solution_values, *model_loader_expected.variables);
+  double expected_objective_value = model_loader_expected.cplex->getObjValue();
+
+  ASSERT_DOUBLE_EQ(model_loader_expected.cplex->getObjValue(), objective_value);
+
+  for (int i = 0; i < solution_values.getSize(); ++i) {
+    IloNum actual_solution_value = solution_values[i];
+    IloNum expected_solution_value = expected_solution_values[i];
+    ASSERT_EQ(actual_solution_value, expected_solution_value);
+  }
+
+  model_loader.environment->end();
+}

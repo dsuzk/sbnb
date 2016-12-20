9a693f861b88a8e52d0eefa067dde40e006e4b7b
Tobias Richter <tr4711@gmail.com>
2016-12-20 17:45:08 +0100

    test(model_loader) calculate expected solution via cplex

--- test/core/branch_and_bound_class_test.cc
+++ test/core/branch_and_bound_class_test.cc
@@ -4,20 +4,42 @@
 #include "core/branch_and_bound_class.h"
 #include "test_models/test_model_loader_class.h"
 
-TEST(BranchAndBoundClass, OptimizationMaxTest) {
+TEST(BranchAndBoundClass, relaxation_test) {
+  TestModelLoader model_loader((char*) "test/test_models/variable_test_model.lp");
 
+  IloNumVarArray vars = *model_loader.variables;
+  IloCplex cplex = *model_loader.cplex;
+
+  for (int i = 0; i < vars.getSize(); ++i) {
+    ASSERT_TRUE(vars[i].getType() == 1); // 1 = INT, 2 = FLOAT, 3 = Bool
+  }
+  cplex.solve();
+  ASSERT_TRUE(cplex.getObjValue() == 2);
+
+  model_loader.model->add(IloConversion(cplex.getEnv(), vars, ILOFLOAT));
+  cplex.solve();
+  ASSERT_TRUE(cplex.getObjValue() == 2.5);
+
+  model_loader.environment->end();
+}
+
+TEST(BranchAndBoundClass, OptimizationMaxTest) {
   TestModelLoader model_loader((char*) "test/test_models/easy_max_model_1.lp");
 
-  BranchAndBound branchAndBound(model_loader.model, model_loader.variables);
-  branchAndBound.optimize();
+  BranchAndBound bnb(model_loader.model, model_loader.variables);
+  bnb.optimize();
+  IloNumArray solution_values = bnb.GetBestSolution();
+  double objective_value = bnb.GetGlobalDualBound();
 
-  IloNumArray solution_values = branchAndBound.GetBestSolution();
-  IloNum expected_solutions[2] = {2, 2};
+  IloNumArray expected_solution_values(*model_loader.expected_solution_values);
+  double expected_objective_value = model_loader.expected_objective_value;
+
+  ASSERT_DOUBLE_EQ(expected_objective_value, objective_value);
 
   for (int i = 0; i < solution_values.getSize(); ++i) {
-    IloNum actual_solution = solution_values[i];
-    IloNum expected_solution = expected_solutions[i];
-    ASSERT_EQ(actual_solution, expected_solution);
+    IloNum actual_solution_value = solution_values[i];
+    IloNum expected_solution_value = expected_solution_values[i];
+    ASSERT_EQ(actual_solution_value, expected_solution_value);
   }
 
   model_loader.environment->end();
@@ -43,24 +65,6 @@ TEST(BranchAndBoundClass, OptimizationMinTest) {
   model_loader.environment->end();
 }
 
-TEST(BranchAndBoundClass, relaxation_test) {
-  TestModelLoader model_loader((char*) "test/test_models/variable_test_model.lp");
-
-  IloNumVarArray vars = *model_loader.variables;
-  IloCplex cplex = *model_loader.cplex;
-
-  for (int i = 0; i < vars.getSize(); ++i) {
-    ASSERT_TRUE(vars[i].getType() == 1); // 1 = INT, 2 = FLOAT, 3 = Bool
-  }
-  cplex.solve();
-  ASSERT_TRUE(cplex.getObjValue() == 2);
-
-  model_loader.model->add(IloConversion(cplex.getEnv(), vars, ILOFLOAT));
-  cplex.solve();
-  ASSERT_TRUE(cplex.getObjValue() == 2.5);
-
-  model_loader.environment->end();
-}
   
 TEST(ModelTest, location_lp) {
   char* model_path = (char*)"test/test_models/location.lp";
@@ -71,15 +75,10 @@ TEST(ModelTest, location_lp) {
   IloNumArray solution_values = bnb.GetBestSolution();
   double objective_value = bnb.GetGlobalDualBound();
 
-  TestModelLoader model_loader_expected(model_path);
-  model_loader_expected.model->add(IloConversion(*model_loader.environment, *model_loader.variables, ILOINT));
-  model_loader_expected.cplex->solve();
+  IloNumArray expected_solution_values(*model_loader.expected_solution_values);
+  double expected_objective_value = model_loader.expected_objective_value;
 
-  IloNumArray expected_solution_values(*model_loader_expected.environment);
-  model_loader_expected.cplex->getValues(expected_solution_values, *model_loader_expected.variables);
-  double expected_objective_value = model_loader_expected.cplex->getObjValue();
-
-  ASSERT_DOUBLE_EQ(model_loader_expected.cplex->getObjValue(), objective_value);
+  ASSERT_DOUBLE_EQ(expected_objective_value, objective_value);
 
   for (int i = 0; i < solution_values.getSize(); ++i) {
     IloNum actual_solution_value = solution_values[i];
@@ -99,15 +98,10 @@ TEST(ModelTest, qpex_lp) {
   IloNumArray solution_values = bnb.GetBestSolution();
   double objective_value = bnb.GetGlobalDualBound();
 
-  TestModelLoader model_loader_expected(model_path);
-  model_loader_expected.model->add(IloConversion(*model_loader.environment, *model_loader.variables, ILOINT));
-  model_loader_expected.cplex->solve();
+  IloNumArray expected_solution_values(*model_loader.expected_solution_values);
+  double expected_objective_value = model_loader.expected_objective_value;
 
-  IloNumArray expected_solution_values(*model_loader_expected.environment);
-  model_loader_expected.cplex->getValues(expected_solution_values, *model_loader_expected.variables);
-  double expected_objective_value = model_loader_expected.cplex->getObjValue();
-
-  ASSERT_DOUBLE_EQ(model_loader_expected.cplex->getObjValue(), objective_value);
+  ASSERT_DOUBLE_EQ(expected_objective_value, objective_value);
 
   for (int i = 0; i < solution_values.getSize(); ++i) {
     IloNum actual_solution_value = solution_values[i];

651e1225ef08d259f0e4ab76a4cc48a9e28ee79e
Tobias Richter <tr4711@gmail.com>
2017-02-06 22:26:32 +0100

    fix(main) ommit cplex output

--- src/main.cc
+++ src/main.cc
@@ -13,6 +13,7 @@ int main(int argn, char* argv[]) {
   IloObjective objective;
 
   try {
+    cplex.setOut(env.getNullStream());
     cplex.importModel(model, lp_file_path, objective, vars, constraints);
     BranchAndBound bnb(&model, &vars);
 
@@ -20,7 +21,7 @@ int main(int argn, char* argv[]) {
     bnb.optimize();
     const float duration = (clock() - begin)/CLOCKS_PER_SEC;
 
-    std::cout << " ------- Branch And Bound Summary ------- " << endl;
+    std::cout << "------- Branch And Bound Summary -------" << endl;
     std::cout << "Objective Value: " << bnb.GetGlobalPrimalBound() << endl;
     std::cout << "Variable Values: " << bnb.GetBestSolution() << endl;
     std::cout << "Elapsed Time: " << duration << " sec" << endl;

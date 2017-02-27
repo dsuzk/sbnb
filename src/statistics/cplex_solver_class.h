7bf1b2a671573f394517421d573c8d7bdb0ee6c2
Tobias Richter <tr4711@gmail.com>
2017-02-27 13:52:57 +0100

    feat(statistics) add cplex solver class

---
+++ src/statistics/cplex_solver_class.h
@@ -0,0 +1,24 @@
+#ifndef __CPLEX_SOLVER_H_
+#define __CPLEX_SOLVER_H_
+
+#include <ilcplex/ilocplex.h>
+
+class CplexSolver {
+  
+ private:
+  IloModel* model_;
+  IloNumVarArray* variables_;
+  IloNumArray best_solution_;
+
+  void SetCplexParams();
+
+ public:
+  CplexSolver(IloModel* model, IloNumVarArray* variables);
+
+  void Solve();
+
+  double global_primal_bound_ = 0;
+  IloCplex cplex_;
+};
+
+#endif // __CPLEX_SOLVER_H_
